c4438b5525b142ee02e76ebec051d0c4ff5662ab
Byron Williams <byron@112percent.com>
2013-11-16 18:20:18 +0000

    Fix columns

--- api.php
+++ api.php
@@ -50,7 +50,8 @@ function get_timeline($from,$to,$since) {
   return $result;
 }
 
-function save_message($conn,$from,$to,$subject,$message) {
+function save_message($from,$to,$subject,$message) {
+  $conn = new PDO($GLOBALS["dsn"], $GLOBALS["username"], $GLOBALS["password"], $GLOBALS["options"]);
   $q = $conn->prepare("INSERT INTO messages (`from_user`,`to_user`,`subject`,`message`,`created_at`) VALUES(?,?,?,?,?)");
   $q->execute(array($from,$to,"",$message,date("Y-m-d H:i:s")));
 
